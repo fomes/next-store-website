d8bbec599d6c93a234a248aa56b89914fab8dd6b
fomes <fgomesdeluna@gmail.com>
2023-11-28 20:04:45 -0300

    Fix btn props

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -4,12 +4,13 @@ import { forwardRef } from "react";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, children, disabled, type = "button" }, ref) => {
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
     return (
       <button
         ref={ref}
         className={cn(
-          "w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition duration-300"
+          "w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transition duration-300",
+          className
         )}
       >
         {children}
